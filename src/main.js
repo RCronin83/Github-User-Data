613cb72327030028b778587ebe384c23ca0385e4
Ryan Cronin <Ryan@Ryans-MBP.home>
2017-03-06 21:10:29 -0500

    stargazer count printed

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ let promise1 = fetch(
       Authorization: 'token ' + process.argv[3]
     },
     body: '...'
-  }
-);
+  } );
 
 promise1.then(function responseHandler(response){
   if (response.status > 199 && response.status < 300){
@@ -19,9 +18,39 @@ promise1.then(function responseHandler(response){
   } else {
     console.log('There was a problem, responseObj.status');
     }
-  } 
-);
-//   }
+  } );
+
+let promise2 = fetch(
+  'https://api.github.com/users/' + process.argv[2] + '/repos',
+  {
+    method: 'GET',
+    headers: {
+      Authorization: 'token ' + process.argv[3]
+    },
+    body: '...'
+  } );
+  promise2.then(function responseHandler(response){
+    if (response.status > 199 && response.status < 300){
+      response.json().then(function printData(githubUserData2){
+        let maxCount = 0;
+        let maxRepoName = '';
+      githubUserData2.forEach(function printNumStargazers(repo){
+        let stargazersCount = repo.stargazers_count;
+        if (maxCount < stargazersCount){
+          maxCount = stargazersCount;
+          maxRepoName = repo.name;
+        }
+      } );
+      console.log(maxRepoName);
+    } );
+
+  } else {
+    console.log('There was a problem', responseObj.status);
+
+  }
+});
+
+// }
 // }
 //
 // );
